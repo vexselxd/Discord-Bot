3f51859714b989f9e1153cb7f9f30ab61a481b6e
vexselxd <medinillavalenzuela@gmail.com>
2025-01-10 13:41:35 -0600

    main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -1,16 +1,32 @@
 import discord
 from discord.ext import commands 
 from bot_logic import gen_pass, game, currency, gen_emoji, flip_coin
-
+#import os
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.members = True 
+intents.message_content = True  
+
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
+@bot.event
+async def on_guild_join(guild):
+    print(f"Me uní al servidor: {guild.name}")
+    channel = next(
+        (chan for chan in guild.text_channels if chan.permissions_for(guild.me).read_messages),
+        None
+    )
+    if channel:
+        await channel.send(f"¡Hola a todos en **{guild.name}**! 🙌\n"
+                           "Gracias por invitarme. Usa `$help` para ver todos mis comandos.")
+
+
+
 
 @bot.command()
 async def hello(ctx):
@@ -40,4 +56,9 @@ async def emoji(ctx):
 async def flip(ctx):
     await ctx.send(flip_coin())
 
-bot.run("token")
+token = "token" 
+
+if token:
+    bot.run(token)
+else:
+    print("Error: No se encontró el token.")
